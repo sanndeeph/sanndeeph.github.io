77f0a9175dd61139127baae4a99ec1dd7a242ddc
sanndeeph <164181374+sanndeeph@users.noreply.github.com>
2024-04-01 17:22:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analysis", "Tableau Dashboards Development", "PowerBI Dashboards Development", "SQL Query optimization", "Automated Reporting Development", "A/B Testing"],
+    strings: ["Business Intelligence (BI) and Analytics","Data Analysis", "Tableau Dashboards Development", "PowerBI Dashboards Development", "SQL Query optimization", "Automated Reporting Development", "A/B Testing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
