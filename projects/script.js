0cc874c4f6a899b04fa9703e0f85d00b0efa5245
sanndeeph <164181374+sanndeeph@users.noreply.github.com>
2024-04-19 03:53:20 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Sandeep H";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Return to the website";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
