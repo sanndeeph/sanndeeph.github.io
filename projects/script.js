8625e69dab5be0202cdebbbf3b9b05c2dc5ed2e3
sanndeeph <164181374+sanndeeph@users.noreply.github.com>
2024-04-24 20:31:08 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -56,7 +56,7 @@ function showProjects(projects) {
           <p>${project.desc}</p>
           <div class="btns">
             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-chart-bar"></i></a>
+            <a href="${project.links.code}" class="btn" target="_blank">Public Viz Platform <i class="fas fa-chart-bar"></i></a>
           </div>
         </div>
       </div>
